01ee9e4f46f13824647889779fbaf7a4aa074662
Andrea Lai <laidevpub@gmail.com>
2019-11-15 12:03:41 +0100

    prove

--- react-geo-app/src/App.js
+++ react-geo-app/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import 'ol/ol.css';
-import 'antd/dist/antd.css';
-import './react-geo.css';
+import "./App.css";
+import "ol/ol.css";
+import "antd/dist/antd.css";
+import "./react-geo.css";
 
-import OlMap from 'ol/Map';
-import OlView from 'ol/View';
-import OlLayerTile from 'ol/layer/Tile';
-import OlSourceOsm from 'ol/source/OSM';
-import OlSourceTileJson from 'ol/source/TileJSON';
-import OlLayerGroup from 'ol/layer/Group';
+import OlMap from "ol/Map";
+import OlView from "ol/View";
+import OlLayerTile from "ol/layer/Tile";
+import OlSourceOsm from "ol/source/OSM";
+import OlSourceXYZ from "ol/source/XYZ";
+import OlSourceVector from "ol/source/Vector"
+import OlFormatGeoJSON from "ol/format/GeoJSON"
+import VectorLayer from 'ol/layer/Vector'
+import OLSourceTileWMS from "ol/source/TileWMS";
+import OlLayerGroup from "ol/layer/Group";
 
-import { Drawer } from 'antd';
+import { Drawer } from "antd";
 import {
   SimpleButton,
   MapComponent,
@@ -22,7 +26,7 @@ import {
   MapProvider,
   mappify,
   onDropAware
-} from '@terrestris/react-geo';
+} from "@terrestris/react-geo";
 
 const MappifiedNominatimSearch = mappify(NominatimSearch);
 const MappifiedMeasureButton = mappify(MeasureButton);
@@ -31,57 +35,76 @@ const Map = mappify(onDropAware(MapComponent));
 
 const layer = new OlLayerTile({
   source: new OlSourceOsm(),
-  name: 'OSM'
+  name: "OSM"
+});
+
+const topoLayer = new OlLayerTile({
+  title: 'OSM',
+  type: 'base',
+  visible: true,
+  source: new OlSourceXYZ({
+      url: 'https://{a-c}.tile.opentopomap.org/{z}/{x}/{y}.png'
+  })
+});   
+
+
+
+const idrometri = new VectorLayer({
+  name: 'IDROMETRI',
+  source: new OlSourceVector({
+    url:
+      "http://a-alai:8080/geoserver/Arpas/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Arpas:idrometri&maxFeatures=2000&outputFormat=application%2Fjson",
+    format: new OlFormatGeoJSON(),
+    crossOrigin: "anonymous"
+  })
+});
+console.log(idrometri)
+
+const mybase = "http://webgis.regione.sardegna.it/geoserver/ows";
+const litologia = new OlLayerTile({
+  name: 'LITOLOGIA',
+  source: new OLSourceTileWMS({
+    url: mybase,
+    params: {
+      LAYERS: "dbu:carta_litologica2019",
+      TILED: true,
+      styles: "quadriUnione"
+    },
+    serverType: "geoserver"
+  })
 });
 
 const layerGroup = new OlLayerGroup({
-  name: 'Layergroup',
+  name: "Arpas",
   layers: [
-    new OlLayerTile({
-      name: 'Food insecurity layer',
-      minResolution: 200,
-      maxResolution: 2000,
-      source: new OlSourceTileJson({
-        url: 'https://api.tiles.mapbox.com/v3/mapbox.20110804-hoa-foodinsecurity-3month.json?secure',
-        crossOrigin: 'anonymous'
-      })
-    }),
-    new OlLayerTile({
-      name: 'World borders layer',
-      minResolution: 2000,
-      maxResolution: 20000,
-      source: new OlSourceTileJson({
-        url: 'https://api.tiles.mapbox.com/v3/mapbox.world-borders-light.json?secure',
-        crossOrigin: 'anonymous'
-      })
-    })
+    litologia,
+    idrometri
   ]
 });
 
-const center = [ 788453.4890155146, 6573085.729161344 ];
-
+const center = [1000000, 4900000];
 const map = new OlMap({
   view: new OlView({
     center: center,
-    zoom: 16,
+    zoom: 8
   }),
-  layers: [layer, layerGroup]
+  layers: [topoLayer, layerGroup]
 });
 
-map.on('postcompose', map.updateSize);
+map.on("postcompose", map.updateSize);
 
 class App extends Component {
-  state = {visible: false};
+  state = { visible: false };
 
   toggleDrawer = () => {
-    this.setState({visible: !this.state.visible});
-  }
+    this.setState({ visible: !this.state.visible });
+  };
 
   render() {
     return (
       <div className="App">
         <MapProvider map={map}>
-          <Map/>
+          <Map />
           <Drawer
             title="react-geo-application"
             placement="right"
@@ -89,9 +112,7 @@ class App extends Component {
             visible={this.state.visible}
             mask={false}
           >
-            <MappifiedNominatimSearch
-              key="search"
-            />
+            <MappifiedNominatimSearch key="search" />
             <MappifiedMeasureButton
               key="measureButton"
               name="line"
@@ -100,12 +121,14 @@ class App extends Component {
             >
               Strecke messen
             </MappifiedMeasureButton>
-            <MappifiedLayerTree
-              layerGroup={layerGroup}
-            />
+            <MappifiedLayerTree layerGroup={layerGroup} />
           </Drawer>
+          <div
+            style={{ position: "fixed", top: "30px", right: "30px" }}
+            onClick={this.toggleDrawer}
+          />
           <SimpleButton
-            style={{position: 'fixed', top: '30px', right: '30px'}}
+            style={{ position: "fixed", top: "80px", right: "80px" }}
             onClick={this.toggleDrawer}
             icon="bars"
           />
@@ -116,4 +139,3 @@ class App extends Component {
 }
 
 export default App;
-
